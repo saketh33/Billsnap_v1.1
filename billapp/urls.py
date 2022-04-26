1c8a4b4a25cfdf621e395666b5802bc462e98997
Sai Pavan Saketh <64682070+saketh33@users.noreply.github.com>
2022-04-26 17:15:00 +0530

    created regsiter

--- billapp/urls.py
+++ billapp/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+from accounts import views as acc_views
+from django.contrib.auth import views as a_views
 
 urlpatterns = [
-    
     path('', views.index, name='index'),
+    path('register', acc_views.RegistrationView.as_view(), name='register'),
+    path('login/', acc_views.LoginView.as_view(), name='login'),
+    path('logout/', acc_views.logout, name='logout'),
+    path('activate/<uidb64>/<token>', acc_views.VerificationView.as_view(), name='activate'),
+    path('reset_password/', a_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset_password'),
+    path('reset_password_sent/', a_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', a_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', a_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), name='password_reset_complete'),
 
 ]
