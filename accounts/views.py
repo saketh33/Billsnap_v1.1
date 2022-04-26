419bd3739605cce30dd0287ac95103ae518921df
Sai Pavan Saketh <64682070+saketh33@users.noreply.github.com>
2022-04-26 17:03:51 +0530

    creating messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views import View
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
-# Create your views here.
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        context = {
+            'username': username,
+            'email': email
+        }
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if len(password) < 6:
+                    messages.error(request, 'Password is too short')
+                    return render(request, 'register.html')
+
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.is_active = False
+                user.save()
+
+                uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+                domain = get_current_site(request).domain
+                link = reverse('activate', kwargs={
+                            'uidb64': uidb64, 'token': token_generator.make_token(user)})
+                email_subject="Activate your Account"
+                activate_url = 'http://'+domain+link
+                email_body = 'Hi, ' + user.username + \
+                    ' Please use this link to verify your account\n' + activate_url
+                email = EmailMessage(
+                    email_subject,
+                    email_body,
+                    'from@example.com',
+                    [email],
+                )
+                email.send(fail_silently=False)
+                messages.success(
+                    request, 'Account successfully created! Check your Email for Account Activation')
+                return redirect('register')
+
+            messages.warning(request, "This Email already exists!")
+            return render(request, 'register.html', context)
+        else:
+            messages.warning(request, "This username already exists!")
+            return render(request, 'register.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
