20b2c56fefc0d71892a435656309603b13b0e540
Sai Pavan Saketh <64682070+saketh33@users.noreply.github.com>
2022-04-28 16:09:10 +0530

    changed login logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+import email
 from django.shortcuts import render,redirect
 from django.views import View
 from django.contrib.auth.models import User
@@ -8,7 +9,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
-from .utils import token_generator
+
 
 class RegistrationView(View):
     def get(self, request):
@@ -110,9 +111,76 @@ class LoginView(View):
             }
             messages.error(request, 'wrong password, try again')
             return render(request, 'login.html', context)
-        
         return render(request, "login.html")
 
 def logout(request):
     auth.logout(request)
     return redirect('index')
+
+from urllib.parse import urlparse, urlunparse
+
+from django.conf import settings
+
+# Avoid shadowing the login() and logout() views below.
+from django.contrib.auth import REDIRECT_FIELD_NAME, get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import ImproperlyConfigured, ValidationError
+from django.http import HttpResponseRedirect, QueryDict
+from django.shortcuts import resolve_url
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.utils.http import url_has_allowed_host_and_scheme, urlsafe_base64_decode
+from django.utils.translation import gettext_lazy as _
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from .utils import token_generator
+
+class PasswordContextMixin:
+    extra_context = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {"title": self.title, "subtitle": None, **(self.extra_context or {})}
+        )
+        return context
+
+from .forms import PasswordResetForm
+
+class PasswordResetView(PasswordContextMixin, FormView):
+    email_template_name = "password_reset_email.html"
+    extra_email_context = None
+    form_class = PasswordResetForm
+    from_email = None
+    html_email_template_name = None
+    subject_template_name = "password_reset_subject.txt"
+    success_url = reverse_lazy("password_reset_done")
+    template_name = "password_reset_form.html"
+    title = _("Password reset")
+    token_generator = default_token_generator     
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        opts = {
+            "use_https": self.request.is_secure(),
+            "token_generator": self.token_generator,
+            "from_email": self.from_email,
+            "email_template_name": self.email_template_name,
+            "subject_template_name": self.subject_template_name,
+            "request": self.request,
+            "html_email_template_name": self.html_email_template_name,
+            "extra_email_context": self.extra_email_context,
+        }
+        form.save(**opts)
+        return super().form_valid(form)
--- billapp/urls.py
+++ billapp/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     path('login/', acc_views.LoginView.as_view(), name='login'),
     path('logout/', acc_views.logout, name='logout'),
     path('activate/<uidb64>/<token>', acc_views.VerificationView.as_view(), name='activate'),
-    path('reset_password/', a_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset_password'),
+    path('reset_password/', acc_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset_password'),
     path('reset_password_sent/', a_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', a_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'), name='password_reset_confirm'),
     path('reset_password_complete/', a_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), name='password_reset_complete'),
-
 ]
